e399a24aa4de29a1fe2f60df03c5070d367a4eed
MegumiAibr <40795944+MegumiAibr@users.noreply.github.com>
2020-07-06 18:23:11 +0900

    UNITY blockout 01
    first commit

---
+++ ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    //GUIで数字を変更できるようにするため,publicを使う
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //floatの場合は数字にfを追加する
+        speed = 10.0f;
+        GetComponent<Rigidbody>().AddForce((transform.right - transform.up) * speed, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
